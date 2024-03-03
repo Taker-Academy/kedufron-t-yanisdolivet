a43e6e29014999381bc2f505d2b878f317a3eacf
yanisdolivet <yanis.dolivet@epitech.eu>
2024-03-03 16:57:05 +0100

    [fix]: total price update

--- JS/manage_shop.js
+++ JS/manage_shop.js
@@ -1,66 +1,75 @@
 const Click_times = new Array();
 const PriceTotal = new Array();
 const count = new Array();
+let total_price = 0;
 
-localStorage.removeItem("cart");
+// localStorage.removeItem("cart");
 
 if (!localStorage.getItem("cart")) {
+    console.log("cart not found!");
     let cart = [];
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 let cart = JSON.parse(localStorage.getItem("cart"));
 
-function IsAlreadyExist(id) {
-    return cart.some(function(dict) {
-        return dict.id === id;
-    });
-}
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem("cart")) {
+        display_shop();
+    }
+});
 
-const AddToShop = async (index)  => {
-    const ContenuPanier = document.getElementsByClassName('contenu_panier')[0];
-    const apercu_shop = document.getElementsByClassName('total_apercu_shop')[0];
+function display_shop() {
     cart.forEach(data => {
-        if ((index + 1) === data.id) {
-            //create elements
-            var ItemContainer = document.createElement('div');
-            var TextContainer = document.createElement('div');
-            var apercu_shop_name = document.createElement('p');
-            var apercu_shop_price = document.createElement('p');
-            var apercu_shop_times = document.createElement('p');
-            var delete_image_apercu = document.createElement('img');
-            var apercu_shop_image = document.createElement('img');
-    
-            ItemContainer.classList.add('items_apercu_shop');
-            ContenuPanier.appendChild(ItemContainer);
-    
-            apercu_shop_image.src = url + 'picture/' + data.id;
-            apercu_shop_image.classList.add('image_item');
-            ItemContainer.appendChild(apercu_shop_image);
-    
-            TextContainer.classList.add('text_apercu_shop');
-            ItemContainer.appendChild(TextContainer);
-            //Assign data
-            if (data.price % 1 == 0) {
-                data.price -= 0.01;
-            }
-            apercu_shop_name.textContent = data.name;
-            apercu_shop_price.textContent = data.price.toFixed(2) + '€';
-            apercu_shop_times.textContent = 'x' + data.times;
-            delete_image_apercu.src = '../ressources/images/poubelle-de-recyclage.png';
-    
-            //Set class name
-            apercu_shop_name.classList.add('apercu_shop_name');
-            apercu_shop_price.classList.add(`apercu_shop_price_${index}`);
-            apercu_shop_times.classList.add(`apercu_shop_time_${index}`);
-    
-            //Append all element
-            TextContainer.appendChild(apercu_shop_name);
-            TextContainer.appendChild(apercu_shop_price);
-            TextContainer.appendChild(apercu_shop_times);
-            TextContainer.appendChild(delete_image_apercu);
+        if (data) {
+            AddToShop(data.id - 1);
+            document.getElementsByClassName('valider_panier')[0].style.visibility = 'visible';
+            total_price += (data.price * data.times);
         }
     });
+    const apercu_shop = document.getElementsByClassName('total_apercu_shop')[0];
+    apercu_shop.textContent = 'Total: ' + Math.round(total_price * 100) / 100 + '€';
+}
+
+function AddToShop(index){
+    var ContenuPanier = document.getElementsByClassName('contenu_panier')[0];
+    //create elements
+    var ItemContainer = document.createElement('div');
+    var TextContainer = document.createElement('div');
+    var apercu_shop_name = document.createElement('p');
+    var apercu_shop_price = document.createElement('p');
+    var apercu_shop_times = document.createElement('p');
+    var delete_image_apercu = document.createElement('img');
+    var apercu_shop_image = document.createElement('img');
+
+    ItemContainer.classList.add('items_apercu_shop');
+    ContenuPanier.appendChild(ItemContainer);
+
+    apercu_shop_image.src = url + 'picture/' + cart[index].id;
+    apercu_shop_image.classList.add('image_item');
+    ItemContainer.appendChild(apercu_shop_image);
+
+    TextContainer.classList.add('text_apercu_shop');
+    ItemContainer.appendChild(TextContainer);
+    //Assign data
+    if (cart[index].price % 1 == 0) {
+        cart[index].price -= 0.01;
+    }
+    apercu_shop_name.textContent = cart[index].name;
+    apercu_shop_price.textContent = cart[index].price.toFixed(2) + '€';
+    apercu_shop_times.textContent = 'x' + cart[index].times;
+    delete_image_apercu.src = '../ressources/images/poubelle-de-recyclage.png';
+
+    //Set class name
+    apercu_shop_name.classList.add('apercu_shop_name');
+    apercu_shop_price.classList.add(`apercu_shop_price_${index}`);
+    apercu_shop_times.classList.add(`apercu_shop_time_${index}`);
+
+    //Append all element
+    TextContainer.appendChild(apercu_shop_name);
+    TextContainer.appendChild(apercu_shop_price);
+    TextContainer.appendChild(apercu_shop_times);
+    TextContainer.appendChild(delete_image_apercu);
 };
 
 const FillPanier = async ()  => {
@@ -69,7 +78,9 @@ const FillPanier = async ()  => {
         const data = response.data;
         data.forEach(data => {
             let index = data._id - 1;
-            count[index] = 1;
+            if (count[index] === undefined) {
+                count[index] = 1;
+            }
             document.getElementsByClassName('buy')[index].onclick = function () {
                 dict = {
                     id: data._id,
@@ -77,16 +88,20 @@ const FillPanier = async ()  => {
                     name: data.name,
                     times: count[index],
                 };
-                cart[index] = dict;
+                if (!cart[index]) {
+                    cart[index] = dict;
+                }
                 document.getElementsByClassName('valider_panier')[0].style.visibility = 'visible';
-                console.log(cart[index].times);
                 if (cart[index].times === 1) {
-                    localStorage.setItem("cart", JSON.stringify(cart));
                     AddToShop(index);
                 } else if (cart[index].times > 1) {
                     document.getElementsByClassName(`apercu_shop_time_${index}`)[0].textContent = 'x' + cart[index].times;
                 }
-                count[index] += 1;
+                total_price += cart[index].price;
+                const apercu_shop = document.getElementsByClassName('total_apercu_shop')[0];
+                apercu_shop.textContent = 'Total: ' + Math.round(total_price * 100) / 100 + '€';
+                localStorage.setItem("cart", JSON.stringify(cart));
+                cart[index].times += 1;
             }
         });
     }
